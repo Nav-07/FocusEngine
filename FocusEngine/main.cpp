31fcb7ff36de71ee9c6c9eea018ce143d1e4d5d0
= <navneil2006@gmail.com>
2020-05-08 08:56:14 +1000

    Bugs...

--- FocusEngine/main.cpp
+++ FocusEngine/main.cpp
@@ -1,1 +1,23 @@
+#include "FocusEngine.hpp"
 
+#include <iostream>
+using namespace std;
+
+class Example : public FocusEngine_EntryPoint {
+protected:
+    void init() override {
+        RenderingContext::getInstance()->init("Example", 640, 480);
+    }
+    void runLoop() override {
+        RenderingContext::getInstance()->pollEvents();
+    }
+    void destroy() override {
+        RenderingContext::getInstance()->destroy();
+    }
+public:
+    Example(): FocusEngine_EntryPoint() {
+    }
+};
+int main() {
+    runGame(new Example(), RenderingContext::getInstance()->isOpen());
+}
