7bdb52a5eda5ebd2809fa9abbe3a7d0df951e176
= <navneil2006@gmail.com>
2020-05-09 21:08:08 +1000

    comments added

--- FocusEngine/FocusEngine/FocusEngine.cpp
+++ FocusEngine/FocusEngine/FocusEngine.cpp
@@ -1,15 +1,23 @@
 #include "FocusEngine.hpp"
 
 void FocusEngine_EntryPoint::constructWindow(const std::string screenTitle, const int screenWidth, const int screenHeight) {
+    // the SDL2 intialize method
+    SDL_Init(SDL_INIT_EVERYTHING);
+    // Create the window based on the parameters passed.
     mWindow = SDL_CreateWindow(screenTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+    // Create the renderer using the window
     mRenderer = SDL_CreateRenderer(mWindow, -1, 0);
+    // Set the window running to true
     mIsRunning = true;
 }
 
 void runGame(FocusEngine_EntryPoint* entryPoint) {
+    // intialize the engine and all the game objects.
     entryPoint->init();
+    // the main game loop
     while (entryPoint->getIsRunning()) {
         entryPoint->runEngine();
     }
+    // destroy the engine
     entryPoint->destroy();
 }
--- FocusEngine/FocusEngine/FocusEngine.hpp
+++ FocusEngine/FocusEngine/FocusEngine.hpp
@@ -2,18 +2,28 @@
 #include <SDL2/SDL.h>
 #include <string>
 
+// The Entry Point for the Game Engine, this single class game engine, has just one abstract class that is used to initialize the basis of the game.
+// This class handles the construction of window and all the objects are instantiated inside this class.
 class FocusEngine_EntryPoint {
 protected:
+    // the SDL_Window* is the SDL object for the window
     SDL_Window* mWindow;
+    // The SDL_Renderer* is the SDL object for the renderer, this is used to call the render functions.
     SDL_Renderer* mRenderer;
+    // This is used to notify the foundation, whether the engine is open or not.
     bool mIsRunning = false;
     
+    // constructWindow is used to intialize the SDL Context and create a window and renderer, this is called in the init() method by the user.
     void constructWindow(const std::string screenTitle, const int screenWidth, const int screenHeight);
 public:
+    // the abstract methods for init(), runEngine(), destroy();
     virtual void init() = 0;
     virtual void runEngine() = 0;
     virtual void destroy() = 0;
+    
+    // This is used by the runGame() function to run the main game loop.
     bool getIsRunning() { return mIsRunning; }
 };
 
+// this is used to run the engine, overriding the FocusEngine_EntryPoint object
 void runGame(FocusEngine_EntryPoint* entryPoint);
